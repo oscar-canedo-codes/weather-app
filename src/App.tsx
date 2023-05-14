6ccd85020f3ec521d033b9887203d0d61437c24e
Oscar Cañedo <oscar.r.canedo@gmail.com>
2023-05-14 17:52:41 +0200

    Refactor for Tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 const App = () => {
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-emerald-900 via-lime-50 to-lime-900 h-[100vh] w-full">
-      <p className="text-9xl font-bold">Hello World!</p>
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
+
+        <h1 className="text-4xl font-bold">Weather Forecast</h1>
+
+      <p className="text-sm mt-2">Search and Select a City</p>
+
+      <div className="flex mt-10 md:mt-4">
+        <input
+        type="text"
+        value={''}
+        className="px-2 py-1 rounded-l-md border-2 border-rose-50"
+      />
+        <button
+        className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer">
+        Search
+        </button>
+      </div>
+      </section>
     </main>
   )
 }
