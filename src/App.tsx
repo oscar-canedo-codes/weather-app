eb5db67db3e515a010bc3a4dcf53f45d541f71e0
Oscar Cañedo <oscar.r.canedo@gmail.com>
2023-05-24 19:08:28 +0200

    Search Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, useEffect, useState } from 'react'
 
 import { optionType } from './types'
+import Search from './components/Search'
 
 const App = (): JSX.Element => {
   const [term, setTerm] = useState<string>('')
@@ -55,41 +56,7 @@ const App = (): JSX.Element => {
 
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-emerald-900 via-lime-50 to-lime-900 h-[100vh] w-full">
-      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
-        <h1 className="text-4xl font-bold">Weather Forecast</h1>
-
-        <p className="text-sm mt-2">Search and Select a City</p>
-
-        <div className="relative flex mt-10 md:mt-4">
-          <input
-            type="text"
-            value={term}
-            className="px-2 py-1 rounded-l-md border-2 border-rose-50"
-            onChange={onInputChange}
-          />
-
-          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
-            {options.map((option: optionType, index: number) => (
-              <li key={option.name + '-' + index}>
-                <button
-                  className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1
-                cursor-pointer"
-                  onClick={() => onOptionSelect(option)}
-                >
-                  {option.name}
-                </button>
-              </li>
-            ))}
-          </ul>
-
-          <button
-            className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer"
-            onClick={onSubmit}
-          >
-            Search
-          </button>
-        </div>
-      </section>
+      <Search term={term} options={options} onInputChange={onInputChange} onOptionSelect={onOptionSelect} onSubmit={onSubmit}/>
     </main>
   )
 }
